2778395ddeec5bc981c7ac606a2cd89cfd3e86e4
EDNI <elvis@opti-mystique.com>
2017-07-14 23:02:24 +0200

    clean useless code

--- wfc.py
+++ wfc.py
@@ -2,23 +2,13 @@ import ssl
 import urllib.request
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup
-#from selenium import webdriver
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-#driver = webdriver.PhantomJS()
-#url = 'https://weather.com/weather/tenday/l/Copenhagen+Denmark+DAXX0009:1:DA'
-#url = 'https://www.wunderground.com/dk/copenhagen'
 url = 'http://www.weather-forecast.com/locations/Copenhagen/forecasts/latest'
-
-#driver.get(url)
-
 uClient = uReq(url)
 page_html = uClient.read()
 uClient.close()
-
-#soup = BeautifulSoup(driver.page_source, "lxml")
-
 soup = BeautifulSoup(page_html, "html.parser")
 
 temperatures = soup.find_all("span",{'class':"temp"})
