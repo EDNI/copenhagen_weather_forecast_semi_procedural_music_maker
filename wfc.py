e1711c9d57e1c917bf74944999749c39d2b68a53
EDNI <elvis@opti-mystique.com>
2017-07-16 01:05:19 +0200

    working partition maker

--- wfc.py
+++ wfc.py
@@ -11,15 +11,41 @@ page_html = uClient.read()
 uClient.close()
 soup = BeautifulSoup(page_html, "html.parser")
 
-temperatures = soup.find_all("span",{'class':"temp"})
-maxtemprow = soup.find_all("tr",{"class":"max-temp-row"})
+maxtemprow = soup.findAll("tr",{"class":"max-temp-row"})
+mintemprow = soup.findAll("tr",{"class":"min-temp-row"})
+humidity = soup.findAll("tr",{"class":"rh"})
 
+filename = "partition.txt"
+f = open(filename, "w")
+i = 1
+
+def format2qlist(var, value):
+  qlistline = "0" + " " + var + str(i) + " " + str(value) + ";"
+  print(qlistline)
+  f.write(qlistline + "\n")
 
 for temps in maxtemprow:
   maxtemp = temps.findAll("span",{"class":"temp"})
-  print (maxtemp)
+  for tempspan in maxtemp:
+    format2qlist("seq", tempspan.text.strip()) 
+    i = i + 2
+
+i = 2
+for temps in mintemprow:
+  mintemp = temps.findAll("span",{"class":"temp"})
+  for tempspan in mintemp:
+    format2qlist("seq", tempspan.text.strip()) 
+    i = i + 2
+
+i = 0
+for humhum in humidity:
+  hum = humhum.findAll("td")
+  for humtd in hum:
+    format2qlist("hum", humtd.text.strip())
+    i = i + 1
 
 
+f.close()
 print ("check")
 
 
